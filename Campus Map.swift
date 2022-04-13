28aaeaeabba23d31f296f4effd8d2b9b7bd5ae5e
AdvaithVijayakumar <102203536+AdvaithVijayakumar@users.noreply.github.com>
2022-04-13 15:39:27 -0700

    Add files via upload

---
+++ Campus Map.swift
@@ -0,0 +1,9 @@
+//
+//  Campus Map.swift
+//  @NCHS
+//
+//  Created by Advaith Vijayakumar on 4/13/22.
+//
+
+import Foundation
+struct 
---
+++ CampusMap.swift
@@ -0,0 +1,20 @@
+//
+//  CampusMap.swift
+//  @NCHS
+//
+//  Created by Advaith Vijayakumar on 4/13/22.
+//
+
+import SwiftUI
+
+struct CampusMap: View {
+    var body: some View {
+        Image("Campus Map")
+    }
+}
+
+struct CampusMap_Previews: PreviewProvider {
+    static var previews: some View {
+        CampusMap()
+    }
+}
