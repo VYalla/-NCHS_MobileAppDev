b50ebebae7e4373d4eb68e95c55c68045e34f508
AdvaithVijayakumar <102203536+AdvaithVijayakumar@users.noreply.github.com>
2022-04-20 14:52:43 -0700

    Update EmailHelper.swift

--- EmailHelper.swift
+++ EmailHelper.swift
@@ -9,24 +9,31 @@ import Foundation
 import MessageUI
 
 class EmailHelper: NSObject, MFMailComposeViewControllerDelegate {
-    public static let shared = EmailHelper()
-    private override init() {
-        //
-    }
     
-    func sendEmail(subject:String, body:String, to:String){
+    // Implement a singleton and private initializer for class EmailHelper.
+    public static let shared = EmailHelper()
+    private override init() {}
+    
+    // Sends an email of the specified subject and body to the specified recipient.
+    func sendEmail(subject:String, body:String, to:String) {
+        
+        // If the current device is able to send email, return.
         if !MFMailComposeViewController.canSendMail() {
             print("No mail account found.")
             return
         }
         
+        // Allocates and initializes an MFMailComposeViewController object.
         let picker = MFMailComposeViewController()
         
+        // Call setter methods to set subject, message body, and recipients.
+        // Use the func parameters as arguments for these method calls.
         picker.setSubject(subject)
         picker.setMessageBody(body, isHTML: true)
         picker.setToRecipients([to])
         picker.mailComposeDelegate = self
         
+        // Call the EmailHelper's getRootViewController and present modifiers to send email.
         EmailHelper.getRootViewController()?.present(picker, animated: true, completion: nil)
     }
     
