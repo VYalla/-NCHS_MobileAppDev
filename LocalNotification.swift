a0bb8491def3c0e33ccf9ee0b0272d28cb758a6b
AdvaithVijayakumar <102203536+AdvaithVijayakumar@users.noreply.github.com>
2022-04-20 14:22:54 -0700

    Update LocalNotification.swift

--- LocalNotification.swift
+++ LocalNotification.swift
@@ -1,5 +1,5 @@
 //
-//  LocalNotificationBootcamp.swift
+//  LocalNotification.swift
 //  LocalNotification
 //
 //  Created by Advaith Vijayakumar on 4/14/22.
@@ -11,50 +11,53 @@ import UserNotifications
 class NotificationManager {
     static let instance = NotificationManager()
     
+    // Requests authorization to interact with user.
     func requestAuthorization() {
+        
+        // Constants for requesting authorization to interact with the user.
         let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        
         UNUserNotificationCenter
             .current()
             .requestAuthorization(options: options) {
-                ( success, error) in
-                if let error = error {
-                    print("ERROR \(error)")
-                } else {
-                    print("SUCCESS")
-                }
+                (success, error) in
         }
     }
     
+    // Schedules a local notification to be delivered to the user's device.
     func scheduleNotification() {
         
         let content = UNMutableNotificationContent()
+        
+        // Set the localized title, subtitle, sound, and badge to the specified values.
         content.title = "CLASS START ALARM"
-        content.subtitle = "[Class name] will start at [class time]."
+        content.subtitle = "Class will start at 8:15. Don't be late!"
         content.sound = .default
         content.badge = 1
         
+        // Allocates a DateComponents object and initializes it.
         var dateComponents = DateComponents()
         
-        /* REPLACE THE FOLLOWING COMMENTS WITH THE CORRECT DATE COMPONENTS
-            Base these components on the student's schedule.*/
-        // dateComponents.hour = [hour]
-        // dateComponents.minute = 17
-        // dateComponents.weekday = [Monday – Friday]
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true
+        // Set the hour and minute of this DateComponents object to the specified values.
+        dateComponents.hour = 8
+        dateComponents.minute = 15
+        
+        // Create a calendar trigger using the date components parameter.
+        let trigger = UNCalendarNotificationTrigger(
+            dateMatching: dateComponents,
+            repeats: true
         )
         
+        // Create a notification request object that you use to schedule a notification.
         let request = UNNotificationRequest(
             identifier: UUID().uuidString,
             content: content,
             trigger: trigger
         )
-        
+    
+        // Schedules a local notification for delivery.
         UNUserNotificationCenter.current().add(request)
         
-    }
-    
-    func cancelNotification() {
-        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
     }
 }
 
@@ -62,6 +65,7 @@ struct LocalNotification: View {
     var body: some View {
         VStack(spacing: 40) {
             
+            // Create buttons for requesting authorization and scheduling notification.
             Button("Request Permission") {
                 NotificationManager.instance.requestAuthorization()
             }
@@ -69,13 +73,11 @@ struct LocalNotification: View {
             Button("Schedule Notification") {
                 NotificationManager.instance.scheduleNotification()
             }
-            
-            Button("Cencel Notification") {
-                NotificationManager.instance.cancelNotification()
-            }
         }
         
         .onAppear {
+            
+            // Set the badge of the app icon on the Home screen.
             UIApplication.shared.applicationIconBadgeNumber =  0
         }
     }
